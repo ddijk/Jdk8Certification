6fec1e301157ce3b310c715c324e2cbba6adbe84
ddijk <dick@dijkrosoft.nl>
2015-10-07 11:48:47 +0200

    add Fibonacci

--- src/main/java/examjava8/Fibonacci.java
+++ src/main/java/examjava8/Fibonacci.java
@@ -5,7 +5,10 @@
  */
 package examjava8;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -18,12 +21,12 @@ public class Fibonacci {
 
     public static void main(String[] args) {
         Fibonacci fibo = new Fibonacci();
-        Arrays.stream(fibo.solve(15, start)).forEach(System.out::println);
+        Arrays.stream(fibo.solveRecursive(15, start)).forEach(System.out::println);
         System.out.println("***");
-        Arrays.stream(fibo.solve2(8)).forEach(System.out::println);
+        Arrays.stream(fibo.solveWithStreams(8)).forEach(System.out::println);
     }
 
-    int[] solve(int n, int[] tussenResultaat) {
+    int[] solveRecursive(int n, int[] tussenResultaat) {
         int len = tussenResultaat.length;
         if (len >= n) {
             return tussenResultaat;
@@ -31,11 +34,23 @@ public class Fibonacci {
             int[] next = Arrays.copyOfRange(tussenResultaat, 0, len + 1);
 
             next[len] = next[len - 1] + next[len - 2];
-            return solve(n, next);
+            return solveRecursive(n, next);
         }
     }
 
-    int[] solve2(int n) {
+    int[] solveWithStreams(int n) {
         return Stream.iterate(new int[]{0, 1}, i -> new int[]{i[1], i[0] + i[1]}).limit(n).mapToInt(j -> j[0]).toArray();
     }
+    
+    List<Integer> solveReduce(int num) {
+        //int result  = Stream.of(1,3,4).reduce(100, (acc, i)-> acc+i).intValue();n
+        List<Integer> initial = new ArrayList<>();
+        initial.add(0);
+        initial.add(1);
+      return Stream.iterate(0, i->i+1).limit(num).reduce(initial, (acc, i)->{int n =acc.get(acc.size()-1)+acc.get(acc.size()-2);acc.add(n); return acc;}, (i,j)-> { i.addAll(j); return i;});
+        
+    //  return result.stream().mapToInt(i->i).sum();
+    }
+    
+    
 }
--- src/test/java/examjava8/FibonacciTest.java
+++ src/test/java/examjava8/FibonacciTest.java
@@ -8,6 +8,7 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -25,7 +26,7 @@ public class FibonacciTest {
 
 
     /**
-     * Test of solve method, of class Fibonacci.
+     * Test of solveRecursive method, of class Fibonacci.
      */
     @Test
     public void testSolve2() {
@@ -34,9 +35,9 @@ public class FibonacciTest {
      
         Fibonacci instance = new Fibonacci();
         int[] expResult = new int[]{0,1};
-        int[] result = instance.solve(n, Fibonacci.start);
+        int[] result = instance.solveRecursive(n, Fibonacci.start);
         assertArrayEquals(expResult, result);
-        assertArrayEquals(expResult, instance.solve2(n));
+        assertArrayEquals(expResult, instance.solveWithStreams(n));
         
     }
     
@@ -47,9 +48,9 @@ public class FibonacciTest {
     
         Fibonacci instance = new Fibonacci();
         int[] expResult = new int[]{0,1,1};
-        int[] result = instance.solve(n, Fibonacci.start);
+        int[] result = instance.solveRecursive(n, Fibonacci.start);
         assertArrayEquals(expResult, result);
-        assertArrayEquals(expResult, instance.solve2(n));
+        assertArrayEquals(expResult, instance.solveWithStreams(n));
         
     }
     
@@ -60,8 +61,17 @@ public class FibonacciTest {
       
         Fibonacci instance = new Fibonacci();
         int[] expResult = new int[]{0,1,1,2,3,5,8,13};
-        int[] result = instance.solve(n, Fibonacci.start);
+        int[] result = instance.solveRecursive(n, Fibonacci.start);
         assertArrayEquals(expResult, result);
-          assertArrayEquals(expResult, instance.solve2(n));
+          assertArrayEquals(expResult, instance.solveWithStreams(n));
+    }
+    
+    @Test
+    public void testReduce() {
+         Fibonacci instance = new Fibonacci();
+        final List<Integer> result = instance.solveReduce(6);
+        System.out.println("result:"+  result);
+         List<Integer> expResult = Arrays.asList(0,1,1,2,3,5,8,13);
+       assertEquals(expResult, result);
     }
 }
