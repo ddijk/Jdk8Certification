a84b36f4380bbc3cbf1a317b69aa29104895af48
Dick Dijk <dick.dijk@ortec-finance.com>
2015-10-08 19:34:47 +0200

    add Fibonacci generator

--- src/test/java/examjava8/FibonacciTest.java
+++ src/test/java/examjava8/FibonacciTest.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import static java.util.stream.Collectors.toList;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -15,5 +16,13 @@ public class FibonacciTest {
         List<Integer> actual = IntStream.iterate(0, n -> n + 2).limit(5).boxed().collect(toList());//forEach(System.out::println);
 
         Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFibo2() {
+        Stream.iterate(new int[]{0, 1}, i -> i)
+                .limit(20)
+                .forEach(t -> System.out.println("(" + t[0] + "," + t[1] + ")"));
+
     }
 }
