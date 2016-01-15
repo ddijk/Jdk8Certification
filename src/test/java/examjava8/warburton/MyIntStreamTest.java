99768fc1962183f18fc1dde7597e383db01af418
Dick Dijk <dick.dijk@ortec-finance.com>
2016-01-15 19:00:39 +0100

    add converson from Double[] to DoubleStream

--- src/test/java/examjava8/warburton/MyIntStreamTest.java
+++ src/test/java/examjava8/warburton/MyIntStreamTest.java
@@ -7,13 +7,12 @@ package examjava8.warburton;
 
 import examjava8.MyIntStream;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import java.util.stream.DoubleStream;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  *
@@ -32,6 +31,22 @@ public class MyIntStreamTest {
         List<Integer> result = instance.generate1();
         assertEquals(expResult, result);
 
+    }
+
+    @Test
+    public void testStreamFromArray() {
+        long n = DoubleStream.of(1.0, 1.2, 1.3).count();
+
+        Double[] da = new Double[]{3.3, 3.4, 2.5};
+        List<Double> doubleList = Arrays.asList(da);
+
+        assertEquals(3.4, Collections.max(doubleList), 0.01d);
+        //     Stream<Double> sd = Arrays.stream(da);
+        assertEquals(3.4d, Arrays.asList(da).stream().max(Comparator.comparingDouble(i -> i.doubleValue())).get(), 0.01d);
+        Comparator<Double> naturalOrder = Comparator.naturalOrder();
+        Comparator<Double> reversedNaturalOrder = naturalOrder.reversed();
+        assertEquals(2.5d, Arrays.asList(da).stream().max(reversedNaturalOrder).get(), 0.01d);
+        assertEquals(3, Arrays.asList(da).stream().count());
     }
 
 }
